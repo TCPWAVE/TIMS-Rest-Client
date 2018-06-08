dba6747c7a0b2eb8a484e1c7154476b1b6e4545c
TCPWAVE <customercare@tcpwave.com>
2018-06-08 13:01:33 +0530

    Create add-DHCP Scope.js

---
+++ add-DHCP Scope.js
@@ -0,0 +1,23 @@
+{
+"method":"POST",
+  "url":"/scope/create",
+  "data":
+  {
+  "update_ns_a":true,
+  "update_ns_ptr":true,
+  "dyn_update_rrs_a":true,
+  "dyn_update_rrs_cname":true,
+  "dyn_update_rrs_ptr":true,
+  "dyn_update_rrs_mx":true,
+  "ttl":"1200",
+  "scope":{"class_code":"3G Phone",
+  "description":"",
+  "organization_name":"EARTH",
+  "primary_dhcp_server":"ngbrdc-qrs02-sl0984",
+  "allocation_type":"dynamic",
+  "template_name":"DHCP Option Template",
+  "allowClassesArray":[],
+  "denyClassesArray":[],
+  "addressRanges":[{"startIP":"10.1.10.201","endIP":"10.1.10.205"}]},
+  "subnetAddress":"10.1.10.0"}
+}
