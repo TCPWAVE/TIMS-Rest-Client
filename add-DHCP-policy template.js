ad87eae9275a7fa1dcd7160906bfa9ee24f15ab7
TCPWAVE <customercare@tcpwave.com>
2018-06-08 12:42:55 +0530

    Create add-DHCP-policy template.js

---
+++ add-DHCP-policy template.js
@@ -0,0 +1,12 @@
+{
+  "method":"POST",
+  "url":"/templates/dhcp-template-create",
+  "data":
+{
+"template_name":"policy-template",
+"description":"",
+"entity_type_code":"POLICY",
+"server_type_code":"DHCP",
+"array_params":[{"display_name":"Default Lease time","value":"3600"}],
+"organization_name":"EARTH"}
+}
