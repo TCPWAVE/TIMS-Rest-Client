1039df39e90b5e0bf7d2ac81c2e7aecd25d2a863
TCPWAVE <customercare@tcpwave.com>
2018-06-08 12:50:01 +0530

    Create edit-domain.js

---
+++ edit-domain.js
@@ -0,0 +1,12 @@
+{
+  "method":"POST",
+  "url":"/domain/update",
+  "data":
+{
+"organization_name": "EARTH",
+"name": "tcpwave1.com",
+"old_name": "tcpwave.com",
+"description": "Update domain using Rest API",
+"updateSubDomain": "true"
+}
+}
