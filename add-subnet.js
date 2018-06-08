c2040936106a1e7c550a0e017a9535b6f0d829a3
TCPWAVE <customercare@tcpwave.com>
2018-06-08 12:08:25 +0530

    Update add-subnet.js

--- add-subnet.js
+++ add-subnet.js
@@ -5,7 +5,7 @@
   {
     "addr1":10, "addr2":1, "addr3":10, "addr4":0, "mask_length":24,
     "name":"Demo Subnet 1", "routerAddress":"10.1.10.1",
-    "network_address":"10.1.10.0", "primary_domain":"qa.com",
-    "organization_name":"QAOrg"
+    "network_address":"10.1.10.0", "primary_domain":"tcpwave.com",
+    "organization_name":"EARTH"
   }
 }
