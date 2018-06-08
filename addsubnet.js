d4a064846d2bdffdb195f74305186dab0167fb33
TCPWAVE <customercare@tcpwave.com>
2018-06-08 12:05:06 +0530

    Create addsubnet.js

---
+++ addsubnet.js
@@ -0,0 +1,11 @@
+{
+  "method":"POST",
+  "url":"subnet/add",
+  "data":
+  {
+    "addr1":10, "addr2":1, "addr3":10, "addr4":0, "mask_length":24,
+    "name":"Demo Subnet 1", "routerAddress":"10.1.10.1",
+    "network_address":"10.1.10.0", "primary_domain":"qa.com",
+    "organization_name":"QAOrg"
+  }
+}
