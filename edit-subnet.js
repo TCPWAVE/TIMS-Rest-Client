d89bd495200b2137b9ed19ff70df8ff3879491f2
TCPWAVE <customercare@tcpwave.com>
2018-06-08 12:59:36 +0530

    Create edit-subnet.js

---
+++ edit-subnet.js
@@ -0,0 +1,22 @@
+{
+"method":"POST",
+  "url":"/subnet/edit",
+  "data":
+  {
+  "address": ["10.1.10.0"],
+  "subnet":{"name":"Demo Subnet 1",
+  "location_id":null,
+  "subnet_group_id":"",
+  "vlan_id":"",
+  "vrf_name":"None",
+  "sharedNetwork":"",
+  "discovery_template":null,
+  "enable_discovery":"no",
+  "description":"",
+  "primary_domain":"tcpwave.com",
+  "routerAddress":"10.1.10.1",
+  "organization_name":"EARTH",
+  "primary_dhcp_server_address":"10.1.10.87",
+  "template_name":"DHCP Option template"}
+  }
+}
